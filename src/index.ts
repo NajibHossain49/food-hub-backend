126e27618a456c4e852c303dee3402a7c0b130e2
NAJIB HOSSAIN <najibhossain1234@gmail.com>
2026-01-10 18:54:27 +0600

    config prismaORM-with PgSQL

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
-import app from "./app";
-import config from "./config/env.config";
+import app from "./app.js";
+import config from "./config/env.config.js";
+import { prisma } from "./lib/prisma.js";
 
-app.listen(config.PORT, () => {
-  console.log(`Server is running on http://localhost:${config.PORT}`);
-  console.log(`Environment: ${config.NODE_ENV}`);
-});
+async function main() {
+  try {
+    await prisma.$connect();
+    console.log("Database connected successfully");
+
+    app.listen(config.PORT, () => {
+      console.log(`Server is running on http://localhost:${config.PORT}`);
+      console.log(`Environment: ${config.NODE_ENV}`);
+    });
+  } catch (error) {
+    console.error("Failed to connect to the database", error);
+    process.exit(1);
+  } finally {
+    process.on("beforeExit", async () => {
+      await prisma.$disconnect();
+    });
+  }
+}
+
+main();
